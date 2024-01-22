d9711c1b16429c119129eb0490b635acd975d003
Suhail Khan <88595134+suhail369@users.noreply.github.com>
2024-01-22 16:13:56 +0530

    Use --no-quarantine

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,9 +4,9 @@ echo "\n<<< Starting homebrew setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat
-brew install google-chrome
-brew install microsoft-edge
-brew install whatsapp
+brew install --no-quarantine httpie
+brew install --no-quarantine bat
+brew install --no-quarantine google-chrome
+brew install --no-quarantine microsoft-edge
+brew install --no-quarantine whatsapp
 
